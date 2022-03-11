47ddaf3465b774a956668a79118cafaf66426ee9
MariaCFFrandsen <56017845+MariaCFFrandsen@users.noreply.github.com>
2022-03-11 10:42:52 +0100

    pow run

--- authenticator/blockchain/utils.go
+++ authenticator/blockchain/utils.go
@@ -1,2 +1,16 @@
 package blockchain
 
+import (
+	"bytes"
+	"encoding/binary"
+	"log"
+)
+
+func ToHex(num int64) []byte {
+	buff := new(bytes.Buffer)
+	err := binary.Write(buff, binary.BigEndian, num)
+	if err != nil {
+		log.Panic(err)
+	}
+	return buff.Bytes()
+}
